b7e1584833486281dd6a1e2dc99b153e7ebf5ce4
amj-web <andrewmjarrett@gmail.com>
2022-03-19 17:10:55 +0000

    Set up game board

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = []
+"""
+Setting up board in the traditional 7 by 7 grid for battle ships.
+Looping the "W" for "water" 7 times to make columns and rows.
+"""
+for x in range(0,7):
+    board.append(["W"] * 7)
+    print(["W"] * 7)
