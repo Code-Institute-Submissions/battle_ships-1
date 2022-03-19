513fda28860e5949adba83e5e70d24f8779677c7
amj-web <andrewmjarrett@gmail.com>
2022-03-19 19:25:09 +0000

    add elif statment to trigger message if use enters the same guess. Add for loop that allows the user to make 7 guesses and finally for loop for turns upto 7 and add if statment to trigger game over message if the user exceeds turn limit of 7

--- run.py
+++ run.py
@@ -1,18 +1,9 @@
-"""
-Import randit from random module built in module in python3 to generate random numbers
-"""
-from random import randint
+from random import randint 
 
 board = []
-"""
-Setting up board in the traditional 7 by 7 grid for battle ships.
-Looping the "W" for "water" 7 times to make columns and rows.
-"""
-for i in range(0,7):
-    board.append(["O"] * 7)
-"""
-Code to loop through each element in outerlist and join method used to combine the items in the list
-"""
+
+for x in range(0, 7):
+  board.append(["O"] * 7)
 
 def print_board(board):
   for row in board:
@@ -20,39 +11,36 @@ def print_board(board):
 
 print_board(board)
 
-"""
-Code to hide battleship in a random location
-"""
-
 def random_row(board):
-  return randint(0, 7)
+  return randint(0, len(board) - 1)
 
 def random_col(board):
-  return randint(0, 7)
+  return randint(0, len(board[0]) - 1)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-
 print (ship_row)
 print (ship_col)
 
-"""
-Code to promt and allow the user to guess the location of a battleship. 
-"""
+for turn in range(7):
+  print ("Turn", turn + 1)
+  guess_row = int(input("Guess Row: "))
+  guess_col = int(input("Guess Col: "))
 
-guess_row = int(input("Guess Row: "))
-guess_col = int(input("Guess Column: "))
-
-
-"""
-Statments for actions after hit or miss 
-"""
-if guess_row == ship_row and guess_col == ship_col:                    
-  print ("YARRR! YOU SUNK ME BATTLESHIP!")
-else:
+  if guess_row == ship_row and guess_col == ship_col:
+    print ("YARRR! YE SUNK MY BATTLESHIP!")
+    break
+  else:
     if guess_row not in range(7) or guess_col not in range(7):
-        print("BE YE BLIND? THAT BE OFF THE MAP!")
+      print ("BE YE BLIND? THAT BE OFF THE MAP!")
+    elif board[guess_row][guess_col] == "X":
+      print("MEMORY OF A GOLDFISH! YE TRIED THAT ALREADY.")
     else:
-        print("MISS! YE BARNACLE BOTTOMED BOZO!")        
-        board[guess_row][guess_col] = "X"
-print_board(board)
+      print("MISS! YE BARNACLE BOTTOMED BOZO!")
+      board[guess_row][guess_col] = "X"
+    if (turn == 6):
+      print ("GAME OVER MAN, GAME OVER!")
+    print_board(board)
+
+
+
