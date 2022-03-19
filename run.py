7e2de81f0d8b3339783d665ad440157e70d399f6
amj-web <andrewmjarrett@gmail.com>
2022-03-19 17:24:55 +0000

    defined print_board and join method used to remove commas and quotation marks

--- run.py
+++ run.py
@@ -3,6 +3,15 @@ board = []
 Setting up board in the traditional 7 by 7 grid for battle ships.
 Looping the "W" for "water" 7 times to make columns and rows.
 """
-for x in range(0,7):
+for i in range(0,7):
     board.append(["W"] * 7)
-    print(["W"] * 7)
+"""
+Custom print and loop to go through each element in outerlist.
+Join method used to combine the items in the list
+"""
+def print_board(board):
+  for row in board:
+    print (" ".join(row))
+
+print_board(board)
+
