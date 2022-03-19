bad03816bdacd61e833391ea457d1a3b8add5bb0
amj-web <andrewmjarrett@gmail.com>
2022-03-19 17:57:05 +0000

    add functions to randomly hide battleship. Add variables to allow user to guess the location of battleship

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+"""
+Import randit from random module built in module in python3 to generate random numbers
+"""
+from random import randint
+
 board = []
 """
 Setting up board in the traditional 7 by 7 grid for battle ships.
@@ -6,12 +11,32 @@ Looping the "W" for "water" 7 times to make columns and rows.
 for i in range(0,7):
     board.append(["W"] * 7)
 """
-Custom print and loop to go through each element in outerlist.
-Join method used to combine the items in the list
+Code to loop through each element in outerlist and join method used to combine the items in the list
 """
+
 def print_board(board):
   for row in board:
     print (" ".join(row))
 
-print_board(board)
+"""
+Code to hide battleship in a random location
+"""
 
+def random_row(board):
+  return randint(0, 7)
+
+def random_col(board):
+  return randint(0, 7)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+print (ship_row)
+print (ship_col)
+
+"""
+Code to allow the user to guess the location of a battleship
+"""
+
+guess_row = int(input("Guess Row: "))
+guess_col = int(input("Guess Column: "))
