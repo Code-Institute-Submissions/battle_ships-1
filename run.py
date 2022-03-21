79b45b10ef8b51cb8f00a057911c0b4dd70d6759
amj-web <andrewmjarrett@gmail.com>
2022-03-21 15:26:55 +0000

    bug fixing and errors. pep8 has cleared code as alright

--- run.py
+++ run.py
@@ -1,62 +1,50 @@
 from random import randint
-"""
-Import randit module
-"""
 
-print("WELCOME TO BATTLESHIP TO PLAY PLEASE SELECT A ROW AND COLUMN BETWEEN 0-6")
+print("TO PLAY, PLEASE SELECT A ROW AND COLUMN BETWEEN 0-6")
 
-"""
-Code to loop through each element in outerlist and join method used to combine the items in the list
-"""
 board = []
 
 for x in range(0, 7):
-  board.append(["O"] * 7)
+    board.append(["O"] * 7)
+
 
 def print_board(board):
-  for row in board:
-    print (" ".join(row))
+    for row in board:
+        print(" ".join(row))
+
 
 print_board(board)
-"""
-Code to hide battleship in a random location
-"""
+
+
 def random_row(board):
-  return randint(0, 7)
+    return randint(0, 7)
+
 
 def random_col(board):
-  return randint(0, 7)
+    return randint(0, 7)
+
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ()
-print ()
-"""
-Code to iterate though 7 turns
-"""
+print()
+print()
+
 for turn in range(7):
-  print ("Turn", turn + 1)
-  guess_row = int(input("Guess Row: "))
-  guess_col = int(input("Guess Col: "))
-  """
-  Code to promt and allow the user to guess the location of a battleship. And actions after each guess.  
-  """
-  if guess_row == ship_row and guess_col == ship_col: #triggered is guess is a hit
-    print ("YARRR! YE SUNK MY BATTLESHIP!")
-    break
-  else:
-    if guess_row not in range(7) or guess_col not in range(7):  #triggered is guess is off the board
-      print ("BE YE BLIND? THAT BE OFF THE MAP!")
-    elif board[guess_row][guess_col] == "X":
-      print("MEMORY OF A GOLDFISH! YE TRIED THAT ALREADY.") #triggered is guess has already been made
+    print("Turn", turn + 1)
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print("YARRR! YE SUNK MY BATTLESHIP!")
+        break
     else:
-      print("MISS! YE BARNACLE BOTTOMED BOZO!") #triggered if guess is a miss
-      board[guess_row][guess_col] = "X"
-    if (turn == 7):
-      print ("GAME OVER MAN, GAME OVER!") #triggered on game end
-    print_board(board)
-
-
-
-
-
+        if guess_row not in range(7) or guess_col not in range(7):
+            print("BE YE BLIND? THAT BE OFF THE MAP!")
+        elif board[guess_row][guess_col] == "X":
+            print("MEMORY OF A GOLDFISH! YE TRIED THAT ALREADY.")
+        else:
+            print("MISS! YE BARNACLE BOTTOMED BOZO!")
+            board[guess_row][guess_col] = "X"
+        if (turn == 7):
+            print("GAME OVER MAN, GAME OVER!")
+        print_board(board)
